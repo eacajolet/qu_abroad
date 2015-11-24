5adbca38e65cb8a527f869ea786ceeee22b906be
Eric <eacajolet@quinnipiac.edu>
2015-11-24 03:47:42 +0000

    Add trips and comments to user profiles

--- core/views.py
+++ core/views.py
@@ -127,3 +127,12 @@ class UserDetailView(DetailView):
     slug_field = 'username'
     template_name = 'user/user_detail.html'
     context_object_name = 'user_in_view'
+    
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        user_in_view = User.objects.get(username=self.kwargs['slug'])
+        trips = Trip.objects.filter(user=user_in_view)
+        context['trips'] = trips
+        comments = Comment.objects.filter(user=user_in_view)
+        context['comments'] = comments
+        return context
